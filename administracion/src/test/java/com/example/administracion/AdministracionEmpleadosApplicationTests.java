d160caaba56e9290f8610a546b394a069a8b81aa
Jorge Daniel Ortega <jorge.d.ortega@unl.edu.ec>
2022-07-10 10:10:55 -0500

    Creacion del proyecto de spring boot

---
+++ administracion/src/test/java/com/example/administracion/AdministracionEmpleadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.administracion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministracionEmpleadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
